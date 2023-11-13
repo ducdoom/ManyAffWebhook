8790de7f507afd3c14c8df683bf736dde3ce4122
Nguyễn Minh Đức <ducdoom@gmail.com>
2023-11-14 00:24:05 +0700

    x

--- ManyAffWebhook/Controllers/ManyAffController.cs
+++ ManyAffWebhook/Controllers/ManyAffController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 
 namespace ManyAffWebhook.Controllers
 {
@@ -12,6 +13,37 @@ namespace ManyAffWebhook.Controllers
         public IActionResult Get()
         {
             return Ok("Success cool!");
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            //read the body of the request as a string
+            string postData = await new StreamReader(Request.Body).ReadToEndAsync();
+            Models.CustomBotTextMsg payload = new Models.CustomBotTextMsg();
+            payload.Content.Text = postData;
+            string jsonString = JsonSerializer.Serialize(payload);
+
+            //send post request to the webhook 
+            string url = "https://open.larksuite.com/open-apis/bot/v2/hook/9aa0f257-55f5-45c8-ae6e-45640640616c";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var response = await client.SendAsync(request);
+            return Ok("send message successfully!");
+
+
+            //try
+            //{
+            //    JsonDocument jsonDocument;
+            //    jsonDocument = await JsonDocument.ParseAsync(Request.Body);
+            //    return Ok(jsonDocument);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return BadRequest(ex.Message);
+            //}
         }
 
 
---
+++ ManyAffWebhook/Models/CustomBotTextMsg.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ManyAffWebhook.Models
+{
+    public class CustomBotTextMsg
+    {
+        [JsonPropertyName("msg_type")]
+        public string MsgType { get; set; } = "text";
+
+        [JsonPropertyName("content")]
+        public Content Content { get; set; } = new();
+    }
+
+    public class Content
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = string.Empty;
+    }
+}
